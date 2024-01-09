b29d3938a8fad3498852ddf42da88700dc06d4ff
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-01-09 17:40:26 -0300

    feat: added middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/index");
+const whitelist = require("./utils/whitelist");
 
-
-
+//Server
 const app = express();
 
+//Middlewares
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+app.disable("x-powered-by");
+app.use(express.json());
+
+//Router
+app.use("/api", router);
 
 module.exports = app;
---
+++ src/utils/whitelist.js
@@ -0,0 +1,3 @@
+const whitelist = ["http://localhost:4321", "https://www.vigi.cam"];
+
+module.exports = whitelist;
