580851962df071f5d5761103d2947ae5c40c0d78
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-01-13 00:53:40 -0300

    feat: add dictionary

---
+++ src/utils/dictionary.js
@@ -0,0 +1,13 @@
+const dictionary = {
+  productDic: {
+    notFound: "Product not found",
+  },
+  userDic: {
+    notFound: "User not found",
+  },
+  orderDic: {
+    notFound: "Order not found",
+  },
+};
+
+module.exports = dictionary;
