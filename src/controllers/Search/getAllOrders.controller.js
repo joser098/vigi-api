8fd8127ee2341f19d2d8337f473f84f8e8e6b2d8
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-01-19 23:55:35 -0300

    Refactor getAllOrders controller to use async/await

--- src/controllers/Search/getAllOrders.controller.js
+++ src/controllers/Search/getAllOrders.controller.js
@@ -3,8 +3,8 @@ const db_conn = require("../../services/db_conn");
 const _getAllOrders = async () => {
   const collection = await db_conn(process.env.DB_NAME, process.env.DB_ORDERS);
 
-  const result = collection.find({}).toArray();
+  const result = await collection.find({}).toArray();
 
   return result;
-};
+}
 module.exports = _getAllOrders;
