ffa20a8079512550fa61ea8238c524e9591cd53a
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-01-19 23:05:20 -0300

    Update cart_addProduct schema validation

--- src/services/zod_schemas/cart_addProduct.schema.js
+++ src/services/zod_schemas/cart_addProduct.schema.js
@@ -1,18 +1,24 @@
 const z = require("zod");
 
 const productSchema = z.object({
-  product_id: z
+  id: z
     .string({
       required_error: "Product ID is required",
       invalid_type_error: "Product ID must be a string",
     })
     .min(24, { message: "Product ID must be at least 24 characters" }),
+  title: z.string({
+    required_error: "Product title is required",
+    invalid_type_error: "Product title must be a string",
+  }),
   quantity: z
     .number({
       required_error: "Quantity is required",
       invalid_type_error: "Quantity must be a number",
     })
-    .min(1, { message: "Quantity must be at least 1" }).positive(),
+    .min(1, { message: "Quantity must be at least 1" })
+    .positive(),
+  unit_price: z.number({ required_error: "Unit price is required" }).positive(),
 });
 
 const cartSchema = z.object({
@@ -28,7 +34,7 @@ const cartSchema = z.object({
 });
 
 const validateCartAddProduct = (input) => {
-    return cartSchema.safeParse(input);
+  return cartSchema.safeParse(input);
 };
 
 module.exports = validateCartAddProduct;
