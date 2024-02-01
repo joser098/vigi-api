34a5652bdb78e7868b3f48a0d2f53b384a1e85f3
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-01 16:40:59 -0300

    Refactor getProductByModel controller to use findOne instead of find

--- src/controllers/Search/getProductByModel.controller.js
+++ src/controllers/Search/getProductByModel.controller.js
@@ -1,11 +1,11 @@
 const db_conn = require("../../services/db_conn");
 
 const _getProductByModel = async (model) => {
-  const regex = new RegExp(model, "i");
+  // const regex = new RegExp(model, "i");
 
   const collection = await db_conn(process.env.DB_NAME, process.env.DB_PRODUCT);
 
-  const result = await collection.find({ model:{ $regex: regex }}).toArray();
+  const result = await collection.findOne({ model: model});
 
   return result;
 };
