d69be5134c336628d80dbe2191a05e447a00b997
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-20 19:00:15 -0300

    localhost redirect fo dev

--- src/handlers/Payment/feedback.handler.js
+++ src/handlers/Payment/feedback.handler.js
@@ -4,7 +4,7 @@ const feedback = async (req, res) => {
 
     return res
       .status(200)
-      .redirect(`https://vigi.cam/payment/${payment_id}`);
+      .redirect(`http://localhost:4321/payment/${payment_id}`);
   } catch (error) {
     return res.status(400).redirect(`http://vigi.cam`);
   }
