59ce85e255ff749cd28996ca2ba53dee52976989
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-22 22:53:58 -0300

    Refactor feedback redirect

--- src/handlers/Payment/feedback.handler.js
+++ src/handlers/Payment/feedback.handler.js
@@ -2,9 +2,13 @@ const feedback = async (req, res) => {
   try {
     const { payment_id, status } = req.query;
 
-    return res
-      .status(200)
-      .redirect(`http://localhost:4321/payment/${payment_id}`);
+    if(status == "approved"){
+      return res
+        .status(200)
+        .redirect(`http://localhost:4321/payment/${payment_id}`);
+    }
+
+    return res.status(200).redirect("http://localhost:4321/perfil")
   } catch (error) {
     return res.status(400).redirect(`http://vigi.cam`);
   }
