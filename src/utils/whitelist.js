1dd77231d8f37805760fcc858ea9bf8a88e35fa1
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-29 12:27:24 -0300

    Add url to whitelist

--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -1,3 +1,3 @@
-const whitelist = ["http://localhost:4321", "https://www.vigi.cam", "https://vigi-git-dev-joser098.vercel.app/"];
+const whitelist = ["http://localhost:4321", "https://www.vigi.cam", "https://vigi-git-dev-joser098.vercel.app", "https://vigi-fb18d4orz-joser098.vercel.app"];
 
 module.exports = whitelist;
