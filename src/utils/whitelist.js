0c5ba8a7c280e7fc083447187ba8a99c795cd17c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-29 12:06:37 -0300

    Add preview  url for cors

--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -1,3 +1,3 @@
-const whitelist = ["http://localhost:4321", "https://www.vigi.cam"];
+const whitelist = ["http://localhost:4321", "https://www.vigi.cam", "https://vigi-git-dev-joser098.vercel.app/"];
 
 module.exports = whitelist;
