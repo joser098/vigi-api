bc08e52a6c8d8edaa4546469e6ef4974d394f208
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-03-06 19:17:28 -0300

    Add token to Create payment Route

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,10 +2,11 @@ const createPaymentOrder = require("../handlers/Payment/createPaymentOrder.handl
 const feedback = require("../handlers/Payment/feedback.handler");
 const getPayment = require("../handlers/Payment/getPayment.handler");
 const receiveWeebhook = require("../handlers/Payment/receiveWebhook.handler");
+const userAuth = require("../middlewares/userAuth")
 
 const paymentRouter = require("express").Router();
 
-paymentRouter.post("/create", createPaymentOrder);
+paymentRouter.post("/create", userAuth, createPaymentOrder);
 paymentRouter.post("/webhook", receiveWeebhook);
 paymentRouter.get("/feedback", feedback);
 paymentRouter.get("/:id", getPayment);
