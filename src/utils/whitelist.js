e64f43e77a15f95228d84da6f8df8fd3e5a609dd
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-03-06 12:51:45 -0300

    Add new domain

--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -1,3 +1,3 @@
-const whitelist = ["http://localhost:4321", "https://www.vigi.cam", "https://vigi-git-dev-joser098.vercel.app", "https://vigi-fb18d4orz-joser098.vercel.app"];
+const whitelist = ["http://localhost:4321", "https://www.vigi.cam", "https://vigi-git-dev-joser098.vercel.app", "https://vigi-fb18d4orz-joser098.vercel.app", "http://192.168.1.2:4321"];
 
 module.exports = whitelist;
