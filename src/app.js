525d6982196636245791905bf7e302c0ef52d9d7
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-03-14 20:51:56 -0300

    FIX CORS

--- src/app.js
+++ src/app.js
@@ -7,17 +7,18 @@ const whitelist = require("./utils/whitelist");
 const app = express();
 
 //Middlewares
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (whitelist.includes(origin) || !origin) {
-        return callback(null, true);
-      }
+app.use(cors({origin: "*"}));
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (whitelist.includes(origin) || !origin) {
+//         return callback(null, true);
+//       }
 
-      return callback(new Error("Not allowed by CORS"));
-    },
-  })
-);
+//       return callback(new Error("Not allowed by CORS"));
+//     },
+//   })
+// );
 app.disable("x-powered-by");
 app.use(express.json());
 
