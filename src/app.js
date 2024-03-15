c3be15a4fb4f0abc22ae2663e8d90ea42e0f385f
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-03-14 21:44:08 -0300

    FIX CORSv4

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const router = require("./routes/index");
 const whitelist = require("./utils/whitelist");
 
@@ -7,26 +7,17 @@ const whitelist = require("./utils/whitelist");
 const app = express();
 
 //Middlewares
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (whitelist.includes(origin) || !origin) {
-//         return callback(null, true);
-//       }
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin) || !origin) {
+        return callback(null, true);
+      }
 
-//       return callback(new Error("Not allowed by CORS"));
-//     },
-//   })
-// );
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
 app.disable("x-powered-by");
 app.use(express.json());
 
--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -1,3 +1,9 @@
-const whitelist = ["http://localhost:4321", "https://www.vigi.cam", "https://vigi-git-dev-joser098.vercel.app", "https://vigi-fb18d4orz-joser098.vercel.app", "http://192.168.1.2:4321"];
+const whitelist = [
+  "http://localhost:4321",
+  "https://www.vigi.cam",
+  "https://vigi-git-dev-joser098.vercel.app",
+  "https://vigi-lvf13s12u-joser098.vercel.app",
+  "http://192.168.1.2:4321",
+];
 
 module.exports = whitelist;
