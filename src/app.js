30566162598dd0b1060c6a742824afa2e7b7ff41
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-03-15 18:20:12 -0300

    fix headers

--- src/app.js
+++ src/app.js
@@ -7,17 +7,31 @@ const whitelist = require("./utils/whitelist");
 const app = express();
 
 //Middlewares
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (whitelist.includes(origin) || !origin) {
-        return callback(null, true);
-      }
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (whitelist.includes(origin) || !origin) {
+//         return callback(null, true);
+//       }
 
-      return callback(new Error("Not allowed by CORS"));
-    },
-  })
-);
+//       return callback(new Error("Not allowed by CORS"));
+//     },
+//   })
+// );
+
+// app.options('/api', cors());
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (whitelist.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
+
+
 app.disable("x-powered-by");
 app.use(express.json());
 
