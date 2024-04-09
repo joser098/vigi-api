6163c688198e5b17122d6c420ef9b0130e5752cd
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-04-09 02:17:13 -0300

    Update whitelist

--- src/utils/whitelist.js
+++ src/utils/whitelist.js
@@ -4,6 +4,8 @@ const whitelist = [
   "https://vigi-git-dev-joser098.vercel.app",
   "https://vigi-lvf13s12u-joser098.vercel.app",
   "http://192.168.1.2:4321",
+  "https://vigi-git-dev-joser098s-projects.vercel.app",
+  "https://vigi-pz4oof859-joser098s-projects.vercel.app"
 ];
 
 module.exports = whitelist;
