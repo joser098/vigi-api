50fb33c92ee432f9a73d14b4e2b8309770f65c85
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-10 03:57:29 -0300

    Add refactor product in promotions sort

--- src/controllers/Search/getProductsInPromotion.controller.js
+++ src/controllers/Search/getProductsInPromotion.controller.js
@@ -3,7 +3,7 @@ const db_conn = require("../../services/db_conn");
 const _getProuctsInPromotion = async () => {
   const collection = await db_conn(process.env.DB_NAME, process.env.DB_PRODUCT);
 
-  const result = await collection.find({ has_promotion: true }).toArray();
+  const result = await collection.find({ has_promotion: true }, { sort: { price: 1 } }).toArray();
 
   return result;
 };
