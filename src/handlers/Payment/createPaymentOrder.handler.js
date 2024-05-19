0ac195223e967308abf389b8075e7ceed3392d69
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-19 00:56:59 -0300

    Refactor create payment order function

--- src/handlers/Payment/createPaymentOrder.handler.js
+++ src/handlers/Payment/createPaymentOrder.handler.js
@@ -1,10 +1,13 @@
+const _getCustomerById = require("../../controllers/Customer/getCustomerById.controller");
 const _createPaymentOrder = require("../../controllers/Payment/createPaymentOrder.controller");
 
 const createPaymentOrder = async (req, res) => {
   try {
     const { customer_id, items, shipments } = req.body;
 
-    const paymentOrder = await _createPaymentOrder(customer_id, items, shipments);
+    const payer = await _getCustomerById(customer_id);
+
+    const paymentOrder = await _createPaymentOrder(payer, items, shipments);
 
     return res.status(200).json({ success: true, data: paymentOrder });
   } catch (error) {
