6a09a87ca51892e3fb5e6c3eca6f1afd83325324
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-19 00:56:31 -0300

    Refactor recieve webhook

--- src/handlers/Payment/receiveWebhook.handler.js
+++ src/handlers/Payment/receiveWebhook.handler.js
@@ -1,10 +1,10 @@
 const { MercadoPagoConfig, Payment } = require("mercadopago");
 const _savePaymentOrder = require("../../controllers/Payment/savePaymentOrder.controller");
-const sendNotification = require("../../controllers/Notifications/sendNotification");
 const createOrderHandler = require("../Order/createOrder.handler");
 const _sendEmail = require("../../controllers/Notifications/sendEmail");
 const { successPayHtml } = require("../../utils/templates/emails");
 const _getCustomerById = require("../../controllers/Customer/getCustomerById.controller");
+const _getOrderByPaymentId = require("../../controllers/Order/getOrderByPaymentId.controller");
 
 const receiveWeebhook = async (req, res) => {
   try {
@@ -15,30 +15,48 @@ const receiveWeebhook = async (req, res) => {
     const mp_payment = new Payment(client);
     const payment = req.query;
 
-    let paymentDetails;
-    if (payment.type === "payment") {
-      paymentDetails = await mp_payment.get({
-        id: payment["data.id"],
-      });
-      _savePaymentOrder(paymentDetails);
-      
-      createOrderHandler(paymentDetails.id ,paymentDetails.additional_info, paymentDetails.transaction_details.total_paid_amount);
-    } else return res.status(204);
-
-    const { user_data } = await _getCustomerById(paymentDetails.additional_info.payer.first_name);
-
-    // Templates
-    const successPay = successPayHtml(user_data.name,paymentDetails.additional_info.items, paymentDetails.transaction_amount, paymentDetails.date_approved, paymentDetails.payment_type_id);
-
-    //Enviar correo al cliente y al admin
-    if (paymentDetails.status === "approved") {
-      _sendEmail(paymentDetails.payer.email, "Pago Exitoso | VIGI", successPay);
-      _sendEmail(process.env.ADMIN_EMAIL, "Nueva venta!", successPay);
+    if (payment.type !== "payment") {
+      return res.status(204).send();
     }
-    
-    return res.status(204);
+
+    const paymentDetails = await mp_payment.get({
+      id: payment["data.id"],
+    });
+
+    //Save the payment order
+    await _savePaymentOrder(paymentDetails);
+
+    //Check if order already exists, not to repeat email notification.
+    const order_exists = await _getOrderByPaymentId(paymentDetails.id);
+
+    //Create order
+    await createOrderHandler(
+      paymentDetails.id,
+      paymentDetails.additional_info,
+      paymentDetails.transaction_details.total_paid_amount
+    );
+
+    //Find customer
+    const customer = await _getCustomerById(paymentDetails.additional_info.payer.last_name);
+
+    // Email HTML Template
+    const successPay = successPayHtml(
+      customer.user_data.name,
+      paymentDetails.additional_info.items,
+      paymentDetails.transaction_amount,
+      paymentDetails.date_approved,
+      paymentDetails.payment_type_id
+    );
+
+    //Send email notification to admin and customer about purchase
+    if (!order_exists && paymentDetails.status === "approved") {
+      await _sendEmail(paymentDetails.payer.email, "Pago Exitoso | VIGI", successPay);
+      await _sendEmail(process.env.ADMIN_EMAIL, "Nueva venta!", successPay);
+    }
+
+    return res.status(204).send();
   } catch (error) {
-    console.log(error);
+    console.eroor(error);
     return res.status(400).json({ message: error.message });
   }
 };
