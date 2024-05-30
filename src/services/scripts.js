f2af137ab053a715c6ec98dd9b40f1b74d9384c9
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-30 03:34:02 -0300

    Fix make integer product price

--- src/services/scripts.js
+++ src/services/scripts.js
@@ -36,7 +36,7 @@ function setPromotionsToProduct(product) {
   if(product.has_promotion && product.discount > 0 && product.discount < 51){
     const price_formated = formatPrice(product.price, product.discount);
 
-    product.price = price_formated.price_discount;
+    product.price = parseInt(price_formated.price_discount);
     product.price_diferred = price_formated.price_diferred;
     product.price_original = price_formated.price_original;
   }
