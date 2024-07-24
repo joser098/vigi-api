0d80aa72ecf7e7aa02abb74e0992cc97d227883b
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-07-24 20:49:05 -0300

    Refactor to find payment by id or order_id

--- src/controllers/Payment/getPayment.controller.js
+++ src/controllers/Payment/getPayment.controller.js
@@ -1,22 +1,14 @@
-// const { Payment, MercadoPagoConfig } = require("mercadopago");
-
 const db_conn = require("../../services/db_conn");
 
-const _getPayment = async (id) => {
-    // const client = new MercadoPagoConfig({
-    //     accessToken: process.env.MP_ACCESS_TOKEN,
-    //   });
-    //   const mp_payment = new Payment(client);
-  
-    //   const payment = await mp_payment.get({
-    //       id
-    //   });
-
-    //   return payment;
-
+const _getPayment = async (id, order_id) => {
     const collection = await db_conn(process.env.DB_NAME,process.env.PAYMENT_ORDERS);
 
-    const result = await collection.findOne({id: Number(id)})
+    let result;
+    if(id) {
+        result = await collection.findOne({id: Number(id)})
+    } else {
+        result = await collection.findOne({ order_id })
+    } 
 
     return result;
 };
